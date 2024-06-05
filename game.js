4967e1f5dee8fb7faed2b0620b81d52462984d3b
ahnafhasnainAiub <ahnafhasnain7860@gmail.com>
2024-06-05 22:56:57 +0600

    Ludo game

---
+++ game.js
@@ -0,0 +1,67 @@
+const dicePhotos = [
+    './images/dice1.jpg', 
+    './images/dice2.jpg',
+    './images/dice3.jpg',
+    './images/dice4.jpg',
+    './images/dice5.jpg',
+    './images/dice6.jpg'
+];
+
+let diceRolls = [];
+
+document.getElementById('startBtn').addEventListener('click', startGame);
+document.getElementById('endBtn').addEventListener('click', endGame);
+
+function startGame() {
+
+    document.getElementById('startBtn').textContent = 'Next Roll';
+
+
+   
+    document.getElementById("diceContainer").classList.add("rotateDice")
+
+
+    setTimeout(()=>{
+        document.getElementById("diceContainer").classList.remove("rotateDice")
+    },1000)
+
+
+     setTimeout(()=>{
+            
+        const currentRoll = Math.floor(Math.random() * 6) + 1;
+        document.getElementById('score').textContent = `This roll: ${currentRoll}`;
+        diceRolls.push(currentRoll);
+    
+        const totalSum = diceRolls.reduce((acc, num) => acc + num, 0);
+        document.getElementById('totalSum').textContent = `Your Total Score: ${totalSum}`;
+    
+    
+        displayImage(currentRoll);
+
+    },1000)
+
+
+}
+
+
+function displayImage(roll) {
+    const img = document.createElement('img');
+    img.src = dicePhotos[roll - 1];
+    img.alt = `Dice showing ${roll}`;
+    
+    const diceContainer = document.getElementById('diceContainer');
+    diceContainer.innerHTML = ''; 
+    diceContainer.appendChild(img);
+}
+
+function endGame() {
+  
+    totalSum.innerHTML = '';
+    score.innerHTML = ''; 
+    diceRolls = [];
+    //document.getElementById('gameOver').textContent = `Game is Over`;
+    document.getElementById('startBtn').textContent = 'Start';
+
+
+
+}
